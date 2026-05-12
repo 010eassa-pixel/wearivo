7f6276da17369e8a5827bf2a2d1848c7b9861d16
wearivo <010eassa@gmail.com>
2026-05-12 23:48:42 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true }
-};
-module.exports = nextConfig;
+  trailingSlash: true, // ده السر اللي هيخلي /admin/ و /category/kids/ يشتغلوا
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
