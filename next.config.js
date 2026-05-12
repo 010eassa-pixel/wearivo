4dac0d294fe2fcfcaac1670bda31747ec2356420
wearivo <010eassa@gmail.com>
2026-05-12 23:29:04 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+// output: 'export = {
   output: 'export', // دي أهم حتة عشان يشتغل كـ Static Site
   images: {
     unoptimized: true, // عشان الصور اللي حطيناها تظهر من غير مشاكل
