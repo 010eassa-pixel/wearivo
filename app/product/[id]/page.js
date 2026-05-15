686480f4a68996a4f2d7ab326ec543a4116cc9de
wearivo <010eassa@gmail.com>
2026-05-15 12:04:23 +0300

    Update page.js

--- app/product/[id]/page.js
+++ app/product/[id]/page.js
@@ -1,15 +1,11 @@
 import ProductDetailsClient from './ProductDetailsClient';
 
-// 1. دي أهم دالة عشان الـ Build بتاع Cloudflare يعدي
 export async function generateStaticParams() {
-  // بنرجع مصفوفة فاضية عشان Next.js ميسألش عن الـ IDs وقت الـ Build
-  return [];
+  return []; 
 }
 
-// 2. بنعرفه إن البيانات ديناميكية وهتيجي من الـ Client
 export const dynamicParams = true;
 
 export default function Page({ params }) {
-  // بنبعت الـ id للملف التاني اللي فيه التصميم والـ use client
   return <ProductDetailsClient id={params.id} />;
 }
