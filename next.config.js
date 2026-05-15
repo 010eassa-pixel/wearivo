55b944d950bae737dd14e436d7c699e1bd724a29
wearivo <010eassa@gmail.com>
2026-05-15 10:38:24 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export'
   trailingSlash: true, // مهم جداً عشان روابط الأقسام والداش بورد
   images: {
     unoptimized: true, // عشان صور Wearivo تظهر بوضوح
