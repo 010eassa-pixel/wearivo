4be726bb964b9f2f3833aa16e9e7b5d1f687ee75
wearivo <010eassa@gmail.com>
2026-05-15 11:04:54 +0300

    Update page.js

--- app/product/[id]/page.js
+++ app/product/[id]/page.js
@@ -1,11 +1,7 @@
 import ProductDetailsClient from './ProductDetailsClient';
 
-// الإعدادات دي لازم تكون في Server Component (بدون use client)
+// التعديل الجوهري: هنخلي الـ runtime فقط ونمسح التانية
 export const runtime = 'edge';
-
-export async function generateStaticParams() {
-  return [];
-}
 
 export default function Page({ params }) {
   return <ProductDetailsClient id={params.id} />;
