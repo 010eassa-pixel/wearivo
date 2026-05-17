8c8dab00db4820e2d526d1f09e754a21ec4ecda5
wearivo <010eassa@gmail.com>
2026-05-17 11:47:44 +0300

    Update page.js

--- app/product/[id]/page.js
+++ app/product/[id]/page.js
@@ -1,13 +1,10 @@
-import ProductDetailsClient from './ProductDetailsClient';
+"use client";
+import { useParams } from "next/navigation";
+import ProductDetailsClient from "./ProductDetailsClient";
 
-// بنقول لـ Next.js متقفلش الـ Build واعتبر الصفحة Static
-export const dynamicParams = false;
+export default function ProductPage() {
+  const params = useParams();
+  const id = params?.id;
 
-export async function generateStaticParams() {
-  // بنرجع مصفوفة وهمية بـ ID واحد بس عشان نعدي بوابات الـ Build
-  return [{ id: '1' }];
-}
-
-export default function Page({ params }) {
-  return <ProductDetailsClient id={params.id} />;
+  return <ProductDetailsClient id={id} />;
 }
